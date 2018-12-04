873cc0fb512d2a2a49e3afd26af966dd860597ad
Jan Jelschen <jan.jelschen@itemis.de>
2018-12-04 11:36:08 +0100

    add missing ng-packagr setup

---
+++ public_api.ts
@@ -0,0 +1,3 @@
+export * from './src/app/modules/event-types-out';
+export * from './src/app/modules/user-activity/user-activity.module';
+export * from './src/app/modules/user-activity/user-activity.service';
